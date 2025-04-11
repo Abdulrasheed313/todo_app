63036bd92ac4d82e56f17028a2806d0c80f95f50
Abdulrasheed313 <rasheedmumna@gmail.com>
2025-04-11 09:45:18 +0500

    send

--- app.js
+++ app.js
@@ -7,17 +7,17 @@ function clr(){
     ul.innerHTML = "";
 }
 
-function edit(e){
+function editTask(e){
 console.log(e)
-    var getValue = prompt("Enter Your Value");
-
+    var getValue = prompt("Enter the updated value",e.parentNode.parentNode.firstChild.textContent);
+    e.parentNode.parentNode.firstChild.textContent = getValue;
 }
 
 function submit(){
     if(inp.value == ""){
         alert("Enter Your Value")
     }else{
-        ul.innerHTML += `<li> ${inp.value} <span> <button class=" btn btn-primary onclick="edit(this)" >Edit</button> <button class="btn btn-secondary" onclick="this.parentNode.parentNode.remove()">Delete</button></span> </li>`;
+        ul.innerHTML += `<li class=" m-2 bg-info text-dark"> ${inp.value} <span> <button class=" btn btn-primary " onclick='editTask(this)' >Edit</button> <button class="btn btn-secondary" onclick="this.parentNode.parentNode.remove()">Delete</button></span> </li>`;
 
         inp.value = "";
     }
